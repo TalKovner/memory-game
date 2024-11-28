65ec5ae3b2d9d572e70949f86be52de35b1548f4
Tal Kovner <elitalkovner@gmail.com>
2024-11-28 02:12:03 +0200

    code styling

--- src/App.js
+++ src/App.js
@@ -1,130 +1,133 @@
-import React from 'react';
-import MemoryCard from './components/MemoryCard';
-import Confetti from './components/Confetti'
-import './style/App.css';
-import data from './data';
+import React from "react";
+import MemoryCard from "./components/MemoryCard";
+import Confetti from "./components/Confetti";
+import "./style/App.css";
+import data from "./data";
 
-window.screen.orientation.lock('portrait')
+window.screen.orientation.lock("portrait");
 
 let cardsData = shuffle(data);
 
 class App extends React.Component {
-	constructor() {
-		super();
-		this.openCards = []
-		this.foundedCount = 0
-		this.flip = this.flip.bind(this);
-		this.state = {
-			memoryCards: cardsData.map(dataObj => <MemoryCard
-				image={dataObj.img}
-				id={dataObj.id} 
-				brotherId={dataObj.brotherId}
-				flipCard={this.flip}
-				key={dataObj.id}
-			/>),
-			throwConfetti: false,
-			data: shuffle(data),
-			foundedCards: []
-		}
-		this.restart = this.restart.bind(this)
-	}
+  constructor() {
+    super();
+    this.openCards = [];
+    this.foundedCount = 0;
+    this.flip = this.flip.bind(this);
+    this.state = {
+      memoryCards: cardsData.map((dataObj) => (
+        <MemoryCard
+          image={dataObj.img}
+          id={dataObj.id}
+          brotherId={dataObj.brotherId}
+          flipCard={this.flip}
+          key={dataObj.id}
+        />
+      )),
+      throwConfetti: false,
+      data: shuffle(data),
+      foundedCards: [],
+    };
+    this.restart = this.restart.bind(this);
+  }
 
-	flip(flipper, restart, markAsFound, id, brotherId, isFound) {
-		if (!isFound) {
-			flipper()
+  flip(flipper, restart, markAsFound, id, brotherId, isFound) {
+    if (!isFound) {
+      flipper();
 
-			this.openCards.push({ id, restart, flipper, brotherId, markAsFound, isFound });
-	
-			if (this.openCards.length === 2) {
-				if (this.openCards[0].id === this.openCards[1].brotherId) {
-					this.handleMatchCards()
-				} else {
-					this.closeOpenCards();
-				}
-	
-				this.openCards = [];
-			}
-		}
-	}
+      this.openCards.push({
+        id,
+        restart,
+        flipper,
+        brotherId,
+        markAsFound,
+        isFound,
+      });
 
-	handleMatchCards() {
-		this.openCards.forEach(card => {
-			card.markAsFound()
-			console.log(card)
-			this.setState((prevState) => ({ foundedCards: [...prevState.foundedCards, card] }));
-			console.log(this.state.foundedCards)
-		});
+      if (this.openCards.length === 2) {
+        if (this.openCards[0].id === this.openCards[1].brotherId) {
+          this.handleMatchCards();
+        } else {
+          this.closeOpenCards();
+        }
 
-		this.foundedCount += 2;
+        this.openCards = [];
+      }
+    }
+  }
 
-		if (this.foundedCount === data.length) {
-			this.setState({ throwConfetti: true })
-		}
-	}
+  handleMatchCards() {
+    this.openCards.forEach((card) => {
+      card.markAsFound();
+      this.setState((prevState) => ({
+        foundedCards: [...prevState.foundedCards, card],
+      }));
+    });
 
-	restart() {
-		this.state.foundedCards.forEach(card => card.restart() || card.flipper())
-		this.openCards.forEach(card => card.restart() || card.flipper())
-		this.openCards = [];
-		this.setState({ foundedCards: [] })
-		cardsData = shuffle(data);
-		this.setState({
-			memoryCards: cardsData.map(
-				dataObj => <MemoryCard
-					image={dataObj.img}
-					id={dataObj.id} 
-					brotherId={dataObj.brotherId}
-					flipCard={this.flip}
-					key={dataObj.id}
-				/>
-			),
-			throwConfetti: false,
-		});
-	}
+    this.foundedCount += 2;
 
-	closeOpenCards() {
-		this.openCards.forEach(card => {
-			setTimeout(card.flipper, 800)
-		});
-	}
+    if (this.foundedCount === data.length) {
+      this.setState({ throwConfetti: true });
+    }
+  }
 
-	render() {
-		// const memoryCards = data.map(dataObj => <MemoryCard
-		// 	image={dataObj.img}
-		// 	id={dataObj.id} 
-		// 	brotherId={dataObj.brotherId}
-		// 	flipCard={this.flip}
-		// 	key={dataObj.id}
-		// />);
-		return (
-			<>
-				{ this.state.throwConfetti ? <Confetti /> : null }
-				<div className='card-container'>
-					{ this.state.memoryCards }
-				</div>
-				<button onClick={() => this.restart()} className="replay-btn"><span class="material-icons">replay</span></button>
-			</>
-		)
-	}
+  restart() {
+    this.state.foundedCards.forEach((card) => card.restart() || card.flipper());
+    this.openCards.forEach((card) => card.restart() || card.flipper());
+    this.openCards = [];
+    this.setState({ foundedCards: [] });
+    cardsData = shuffle(data);
+    this.setState({
+      memoryCards: cardsData.map((dataObj) => (
+        <MemoryCard
+          image={dataObj.img}
+          id={dataObj.id}
+          brotherId={dataObj.brotherId}
+          flipCard={this.flip}
+          key={dataObj.id}
+        />
+      )),
+      throwConfetti: false,
+    });
+  }
+
+  closeOpenCards() {
+    this.openCards.forEach((card) => {
+      setTimeout(card.flipper, 800);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.throwConfetti ? <Confetti /> : null}
+        <div className="card-container">{this.state.memoryCards}</div>
+        <button onClick={() => this.restart()} className="replay-btn">
+          <span class="material-icons">replay</span>
+        </button>
+      </>
+    );
+  }
 }
 
 function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
-  
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-  
-	  // Pick a remaining element...
-	  randomIndex = Math.floor(Math.random() * currentIndex);
-	  currentIndex -= 1;
-  
-	  // And swap it with the current element.
-	  temporaryValue = array[currentIndex];
-	  array[currentIndex] = array[randomIndex];
-	  array[randomIndex] = temporaryValue;
-	}
-  
-	return array;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 export default App;
--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,57 +1,73 @@
-import React from 'react';
-import '../style/MemoryCard.css'
-import back from '../pics/backCover.jpg';
+import React from "react";
+import "../style/MemoryCard.css";
+import back from "../pics/backCover.jpg";
 
 class MemoryCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            deg: 0,
-            isFound: false
-        }
-        this.rotation = getRandomInt(-5, 5);
-        this.flipper = this.flipper.bind(this);
-        this.markAsFound = this.markAsFound.bind(this);
-        this.restart = this.restart.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      deg: 0,
+      isFound: false,
+    };
+    this.rotation = getRandomInt(-5, 5);
+    this.flipper = this.flipper.bind(this);
+    this.markAsFound = this.markAsFound.bind(this);
+    this.restart = this.restart.bind(this);
+  }
 
+  flipper() {
+    this.setState((prevState) => {
+      document.getElementById(
+        this.props.id
+      ).style.transform = `rotateY(${Math.abs(prevState.deg - 180)}deg)`;
+      return { deg: Math.abs(prevState.deg - 180) };
+    });
+  }
 
+  markAsFound() {
+    this.setState({ isFound: true });
+  }
 
-    flipper() {
-        this.setState(prevState => {
-            document.getElementById(this.props.id).style.transform = `rotateY(${Math.abs(prevState.deg - 180)}deg)`;
-            return { deg: Math.abs(prevState.deg - 180) };
-        });
-    }
+  restart() {
+    this.setState({ isFound: false });
+  }
 
-    markAsFound() {
-        this.setState({ isFound: true })
-    }
-
-    restart() {
-        this.setState({ isFound: false })
-    }
-
-    render() {
-        return (
-            <div className="flip-card" style={{transform: `rotate(${this.rotation}deg)`}} >
-                <div id={this.props.id} className="flip-card-inner" onClick={() => this.props.flipCard(this.flipper, this.restart, this.markAsFound, this.props.id, this.props.brotherId, this.state.isFound)}>
-                    <div className="flip-card-front">
-                        <img src={back} alt="Avatar" />
-                    </div>
-                    <div className="flip-card-back">
-                        <img src={this.props.image} alt="Avatar" />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        className="flip-card"
+        style={{ transform: `rotate(${this.rotation}deg)` }}
+      >
+        <div
+          id={this.props.id}
+          className="flip-card-inner"
+          onClick={() =>
+            this.props.flipCard(
+              this.flipper,
+              this.restart,
+              this.markAsFound,
+              this.props.id,
+              this.props.brotherId,
+              this.state.isFound
+            )
+          }
+        >
+          <div className="flip-card-front">
+            <img src={back} alt="Avatar" />
+          </div>
+          <div className="flip-card-back">
+            <img src={this.props.image} alt="Avatar" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export default MemoryCard;
